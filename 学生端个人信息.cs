8d4122a4e5f38872fae60fc675b59671f5c48291
ykai-ml <1720167288@qq.com>
2024-06-21 11:37:00 +0800

    学生端个人信息

---
+++ 学生端个人信息.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 学生成绩管理系统
+{
+    public partial class 学生端个人信息 : Form
+    {
+        public 学生端个人信息()
+        {
+            InitializeComponent();
+        }
+
+        private void 学生端个人信息_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
